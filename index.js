0f754cc1dd0d4f3db565c9a7d5d0a7c335b36079
shueb <ssuhebbare@gmail.com>
2026-06-26 19:08:16 +0300

    added files and styled bottom nav

--- index.js
+++ index.js
@@ -44,6 +44,98 @@ AOS.init({
   anchorPlacement: "top-bottom",
 });
 
+// rendering
+const foods = [
+{
+  // cakes
+  id: 1,
+  image: "./images/choclatecake.jpg",
+  name: "chocolate cake",
+  category: "cake",
+  type: "chocolate",
+  desc: "Rich, moist, and packed with deep chocolate flavor. Freshly baked and beautifully decorated, perfect for birthdays, celebrations, or satisfying your chocolate cravings.",
+  price: 'Kshs.' + 250,
+},
+{
+  id: 2,
+  name: "Vanilla cake",
+  image: "./images/vanillacake.jpg",
+  category: "cake",
+  type: "vanilla",
+  name: "Vanilla Cake",
+  desc: "Light, fluffy, and perfectly sweet. Made with premium ingredients and ideal for custom designs, birthdays, and family celebrations.",
+  price: 250,
+},
+{
+  id: 3,
+  image: "./images/redvalletcake.jpg",
+  name: "Red Velvet Cake",
+  category: "cake",
+  type: "cheese",
+  desc: "Soft, velvety layers with smooth cream cheese frosting. A classic favorite that adds elegance and sweetness to every special occasion.",
+  price: "kshs." + 250,
+},
+{
+  id: 4,
+  image: "./images/blackcake.jpg",
+  name: "Black Forest Cake",
+  category: "cake",
+  type: "chocolate",
+  desc: "Layers of chocolate sponge, fresh cream, and cherries combined into a rich and delightful cake that everyone will love.",
+  price: "kshs." + 250,
+},
+{
+  id: 5,
+  image: "./images/strawberrycake.jpg",
+  name: "strawberry cake",
+  category: "cake",
+  type: "strawberry",
+  desc: "Fresh strawberry flavor blended with soft sponge and creamy frosting for a refreshing and delicious dessert experience.",
+  price: "kshs." + 250,
+},
+
+];
+console.log("now on render")
+const renderedItems = (item) => {
+  const container = document.getElementById("foodContainer");
+
+  container.innerHTML = "";
+
+  item.forEach((food) => {
+    container.innerHTML += `
+           <div class="myWork-item" data-aos="fade-up">
+              <div class="myWork-img">
+                <img src="${food.image}" alt="${food.name}" />
+              </div>
+              <div class="myWork-info">
+                <h2 class="myWork-item-title">${food.name}</h2>
+                <p class="myWork-item-desc">
+                  ${food.desc}
+                </p>
+                <h1 class="myWork-item-price">${food.price}</h1>
+               <a href="order.html?item=${encodeURIComponent(food.name)}&price=${food.price}"
+             class="primary-btn btn">
+            order now
+          </a>
+              </div>
+            </div>
+    `;
+  });
+};
+
+renderedItems(foods);
+
+
+const cakeButton = document.getElementById("cakes");
+
+cakeButton.addEventListener("click", () => {
+  const cakes = foods.filter((food) => {
+    return food.type === "chocolate";
+  })
+
+  renderedItems(cakes);
+});
+
 const btn = document.getElementById("copyBtn");
 
 btn.addEventListener("click", async () => {
@@ -74,54 +166,7 @@ form.addEventListener("submit", (e) => {
   const whatsApp = `New order:
   Name: ${name}
   Phone Number: ${number}
-  location: 
+  location: ${location}
   `;
 });
 console.log("starting");
-const cards = [
-{
-  id: 1,
-  image: "./images/meatrice.png",
-  name: "Rice and chicken",
-  desc: "Freshly prepared rice and chicken served with a complimentary juice and fresh chili at no extra cost.",
-  price: 250,
-},
-{
-  id: 1,
-  image: "./images/choclatecake.jpg",
-  name: "Rice and chicken",
-  desc: "Freshly prepared rice and chicken served with a complimentary juice and fresh chili at no extra cost.",
-  price: 250,
-},
-];
-console.log("now on render")
-const renderedItems = (item) => {
-  const container = document.getElementById("foodContainer");
-
-  container.innerHTML = "";
-
-  item.forEach((food) => {
-    container.innerHTML += `
-           <div class="myWork-item" data-aos="fade-up">
-              <div class="myWork-img">
-                <img src="${food.image}" alt="${food.name}" />
-              </div>
-              <div class="myWork-info">
-                <h2 class="myWork-item-title">${food.name}</h2>
-                <p class="myWork-item-desc">
-                  ${food.desc}
-                </p>
-                <h1 class="myWork-item-price">${food.price}</h1>
-               <a href="order.html?item=${encodeURIComponent(food.name)}&price=${food.price}"
-             class="primary-btn btn">
-            order now
-          </a>
-              </div>
-            </div>
-    `;
-  });
-};
-
-renderedItems(cards);
-console.log("ends here")
-console.log("container:", document.getElementById("foodContainer"));
